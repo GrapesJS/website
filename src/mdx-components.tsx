dfadfa339eb544e903f5b7990c06932b427a1c4c
Artur Arseniev <artur.catch@hotmail.it>
2024-08-06 15:55:48 +0400

    Add Meet Studio SDK blog post

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,35 +1,46 @@
 import markdownStyles from "@/app/_components/markdown-styles.module.css";
-import type { MDXComponents } from 'mdx/types';
-import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc';
+import type { MDXComponents } from "mdx/types";
+import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import Link from "next/link";
 import { slugify } from "./lib/utils";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-  }
+  };
 }
 
 const mdxComponents = {
-  a: ({ children, ...rest}: any) => <Link className="link-gjs" {...rest}>{ children }</Link>,
-  img: ({ children, ...rest}: React.HTMLProps<any>) => <img className="rounded-md shadow-md" {...rest}>{children}</img>,
-  h1: ({ children, ...rest}: React.HTMLProps<any>) => (
+  a: ({ children, ...rest }: any) => (
+    <Link className="link-gjs" {...rest}>
+      {children}
+    </Link>
+  ),
+  strong: ({ children, ...rest }: React.HTMLProps<any>) => (
+    <b className="font-medium" {...rest}>
+      {children}
+    </b>
+  ),
+  img: ({ children, ...rest }: React.HTMLProps<any>) => (
+    <img className="rounded-md shadow-md" {...rest}>
+      {children}
+    </img>
+  ),
+  h1: ({ children, ...rest }: React.HTMLProps<any>) => (
     <h1 className="font-bold text-xl" id={slugify(children!)} {...rest}>
-      <a href={`#${slugify(children!)}`}>{ children }</a>
+      <a href={`#${slugify(children!)}`}>{children}</a>
     </h1>
+  ),
+  h2: ({ children, ...rest }: React.HTMLProps<any>) => (
+    <h2 className="font-semibold" id={slugify(children!)} {...rest}>
+      <a href={`#${slugify(children!)}`}>{children}</a>
+    </h2>
   ),
 };
 
 export function ContainerMDX({ source }: MDXRemoteProps) {
   // @ts-ignore
-  const result = <MDXRemote
-    components={mdxComponents}
-    source={source}
-  />;
+  const result = <MDXRemote components={mdxComponents} source={source} />;
 
-  return (
-    <div className={markdownStyles["markdown"]}>
-        {result}
-    </div>
-  );
+  return <div className={markdownStyles["markdown"]}>{result}</div>;
 }
