d348bb892b220275a18d212fbe7f51e830b71182
Artur Arseniev <artur.catch@hotmail.it>
2024-10-21 13:40:52 +0400

    Use grapesjs from npm

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,40 +2,60 @@ import Link from "next/link";
 import cx from "classnames";
 import { GRAPESJS_REPO, GRAPESJS_X } from "@/lib/constants";
 import Icon from "@mdi/react";
-import { mdiGithub } from '@mdi/js';
+import { mdiGithub } from "@mdi/js";
 
-const clsContainer = 'container mx-auto px-5';
-const clsHeadInner = 'flex flex-wrap items-center md:justify-between gap-7 flex-col md:flex-row';
+const clsContainer = "container mx-auto px-5";
+const clsHeadInner =
+  "flex flex-wrap items-center md:justify-between gap-7 flex-col md:flex-row";
 
 interface HeaderProps extends React.HTMLProps<HTMLElement> {
-  isHome?: boolean,
-  transparent?: boolean,
+  isHome?: boolean;
+  transparent?: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({isHome, className, transparent, ...rest }) => {
+const Header: React.FC<HeaderProps> = ({
+  isHome,
+  className,
+  transparent,
+  ...rest
+}) => {
   const navLinks = [
-    { content: 'Careers', href: '/careers' },
-    { content: 'Blog', href: '/blog' },
-    { content: 'Docs', href: '/docs' },
+    { content: "Careers", href: "/careers" },
+    { content: "Blog", href: "/blog" },
+    { content: "Docs", href: "/docs" },
     // { content: 'Blog', href: '/blog' },
-    { content: <Icon path={mdiGithub} size={1}/>, href: GRAPESJS_REPO, target: '_blank' },
-    { content: <span className="text-xl">𝕏</span>, href: GRAPESJS_X, target: '_blank' },
+    {
+      content: <Icon path={mdiGithub} size={1} />,
+      href: GRAPESJS_REPO,
+      target: "_blank",
+    },
+    {
+      content: <span className="text-xl">𝕏</span>,
+      href: GRAPESJS_X,
+      target: "_blank",
+    },
   ];
 
   if (isHome) {
     navLinks.unshift(
-      { content: 'Support', href: '#support' },
-      { content: 'Features', href: '#features' },
-      { content: 'Libre Friends', href: '/libre-friends/' }
-    )
+      { content: "Support", href: "#support" },
+      { content: "Features", href: "#features" }
+    );
   }
 
   return (
-    <header className={cx(className, !transparent && 'bg-gjs')} {...rest}>
-      <div className={cx(clsContainer, clsHeadInner, 'py-4')}>
+    <header className={cx(className, !transparent && "bg-gjs")} {...rest}>
+      <div className={cx(clsContainer, clsHeadInner, "py-4")}>
         <div className="relative">
-          <Link id="logo-cont" href="/" className="select-none flex gap-3 items-center">
-            <img className="h-[40px]" src="/assets/images/grapesjs-logo-cl.png"/>
+          <Link
+            id="logo-cont"
+            href="/"
+            className="select-none flex gap-3 items-center"
+          >
+            <img
+              className="h-[40px]"
+              src="/assets/images/grapesjs-logo-cl.png"
+            />
             <span className="text-3xl">GrapesJS</span>
           </Link>
         </div>
