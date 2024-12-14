d6b81c0b19efa1efb0edb70a09d53d777627a6c9
Artur Arseniev <artur.catch@hotmail.it>
2024-12-14 12:54:18 -0300

    Up hero outline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function HomePage() {
 
           <StarOnGH />
 
-          <div className="flex flex-col w-full rounded-t-[16px] outline outline-8 outline-[#ffffff33] bg-[#252527]">
+          <div className="flex flex-col w-full rounded-t-[16px] outline outline-4 outline-[#ffffff33] bg-[#252527]">
             <div className="flex gap-[8px] h-[30px] items-center px-4">
               <div className="w-[12px] h-[12px] rounded-full bg-[#ED6D60]"></div>
               <div className="w-[12px] h-[12px] rounded-full bg-[#F6BF52]"></div>
