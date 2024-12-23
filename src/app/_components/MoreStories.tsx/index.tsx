1d55d58c0cd084eda75c1b50a2dfd150481ab66d
David Villamizar <davl3232@gmail.com>
2024-12-23 15:14:47 -0500

    fix gap between posts

--- src/app/_components/MoreStories.tsx/index.tsx
+++ src/app/_components/MoreStories.tsx/index.tsx
@@ -10,7 +10,7 @@ export function MoreStories({ posts }: Props) {
       <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
         More Stories
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-[32px] gap-y-[48px] mb-32">
         {posts.map((post) => (
           <PostPreview key={post.slug} post={post} />
         ))}
