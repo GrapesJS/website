ffd9f0cc39b0b30be118c2007f31e4aba49eed6a
Artur Arseniev <artur.catch@hotmail.it>
2025-03-28 16:34:13 +0400

    Add shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function slugify(str: any) {
   return String(str)
     .normalize("NFKD") // split accented characters into their base characters and diacritical marks
@@ -7,4 +10,8 @@ export function slugify(str: any) {
     .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
     .replace(/\s+/g, "-") // replace spaces with hyphens
     .replace(/-+/g, "-"); // remove consecutive hyphens
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
