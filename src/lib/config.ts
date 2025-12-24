376a6f489e8e53a0b671e23debbd757daf4dc28e
Can Urgan <can@grapesjs.com>
2025-12-24 12:31:08 +0300

    default to localhost

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,9 @@
 export const getAppApiBase = (): string => {
-  return process.env.NEXT_PUBLIC_API_APP_BASE || 'http://app.grapesjs.local:3000';
+  return process.env.NEXT_PUBLIC_API_APP_BASE || 'http://localhost:3000';
 };
 
 export const getWwwUrl = (): string => {
-  return process.env.NEXT_PUBLIC_WWW_URL || 'http://www.grapesjs.local:3001';
+  return process.env.NEXT_PUBLIC_WWW_URL || 'http://localhost:3001';
 };
 
 export const isDevelopment = (): boolean => {
